ba61901c385af3941670916af0dca1dab3f4532d
anoyetta <anoyetta@gmail.com>
2019-01-24 20:11:06 +0900

    最大化、最小化ボタンを表示した

--- FFXIVZoomHack/Properties/AssemblyInfo.cs
+++ FFXIVZoomHack/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("cf1b4d2e-dc50-4e18-9942-d2101e721954")]
-[assembly: AssemblyVersion("1.3.0.1")]
-[assembly: AssemblyFileVersion("1.3.0.1")]
+[assembly: AssemblyVersion("1.3.0.2")]
+[assembly: AssemblyFileVersion("1.3.0.2")]
